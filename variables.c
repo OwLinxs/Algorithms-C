49ec56252bf32a67baa2a521cad387179e0ce97c
OwLin <owlin@172-14-15-90.lightspeed.sntcca.sbcglobal.net>
2026-02-23 18:23:51 -0300

    variables

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int variavel1 = 10;
+  int variavel2 = 20;
+  char letra = 'c';
+  float variavel3 = 10.1516;
+  printf("%d\n", variavel1);
+  printf("%d\n", variavel2);
+  printf("%c\n", letra);
+  printf("%f\n", variavel3);
+
+  return 0;
+}
